4b7ec63d4a3563d88c37c72b9c18d30e713707a6
Durvesh Gambhe <74155862+D-Sensei@users.noreply.github.com>
2023-03-24 12:45:21 +0530

    Add files via upload

--- Windows/Antivirus.py
+++ Windows/Antivirus.py
@@ -1,1 +1,36 @@
+import os
+import hashlib
 
+# Define the scope of the scanner
+SCAN_PATHS = ['C:\\', 'D:\\']
+FILE_TYPES = ['.exe', '.dll', '.sys', '.bat']
+
+# Define the malware signature database
+MALWARE_HASHES = [
+    'c95aa409b6d299c0298d3a8e9b71cd97', # Trojan.GenericKD.30132277
+    '0bfcf643e67928cc09e202d840d220f2', # W32.Sality.AE
+    '026f8c2f58ebcac4f6fae1d1722d1e3e'  # Worm.Autorun.VHG
+]
+
+def scan_file(file_path):
+    # Compute the hash value of the file
+    with open(file_path, 'rb') as f:
+        file_content = f.read()
+    file_hash = hashlib.md5(file_content).hexdigest()
+    
+    # Check if the hash value matches any known malware
+    if file_hash in MALWARE_HASHES:
+        print(f'Found malware: {file_path}')
+        # Implement a quarantine or removal mechanism here
+
+def scan_files():
+    # Search for files in the specified locations
+    for scan_path in SCAN_PATHS:
+        for root, dirs, files in os.walk(scan_path):
+            for file_name in files:
+                if any(file_name.endswith(file_type) for file_type in FILE_TYPES):
+                    file_path = os.path.join(root, file_name)
+                    scan_file(file_path)
+
+if __name__ == '__main__':
+    scan_files()
